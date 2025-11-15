715989374414780ad07e8ffba7af61f875c630f1
ifaamrillah <amrillah.work@gmail.com>
2025-11-15 12:33:27 +0700

    init project

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from "react-router";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
