ed84d16e07aacefb0b9df9443d847bcf286c6be9
muafalah <muh.ifa.amrillah@gmail.com>
2025-11-17 19:10:41 +0700

    feat: add dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 import LoginPage from "@/pages/auth/login";
+import DashboardPage from "@/pages/dashboard";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Rute Publik */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Rute Terproteksi */}
+        {/* <Route element={<ProtectedRoute />}> */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* </Route> */}
+
+        {/* Redirect default */}
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
   );
